f51f9ceacdebd8b0673ca29fd30534436bc448af
Nick Stracke <nick.stracke@web.de>
2022-03-07 12:09:39 +0530

    update mvtech flags

--- tf2/scripts/mvtech_run.sh
+++ tf2/scripts/mvtech_run.sh
@@ -1,7 +1,11 @@
+categories=[Bottle,Pill,Tile,Transistor,Zipper,Hazelnut,metal_nut,Toothbrush,Wood]
+
 python3 run.py --dataset=mvtech --data_dir="/mnt/data" --eval_split=test \
---use_tpu=False --model_dir=logs --mode=train_then_eval --train_mode=pretrain --image_size=224 \
---image_size=224 --train_batch_size=128 --lineareval_while_pretraining=False --test_perc=0.3 \
---eval_batch_size=128 --load_existing_split=False --anomaly_perc=0.15 --run_id=001
+--model_dir=logs --mode=train_then_eval --train_mode=pretrain --image_size=224 \
+--image_size=224 --train_batch_size=128 --lineareval_while_pretraining=True --test_perc=0.3 \
+--eval_batch_size=256 --load_existing_split=False --anomaly_perc=0.15 --run_id=001 \
+--categories=$categories --train_epochs=200
+
 
 
 # --train_mode= pretrain or finetune

3467b7ff57502c3682276341aa901875f628dd62
Nick Stracke <nick.stracke@web.de>
2022-03-29 11:18:36 +0200

    update mvtech command

--- tf2/scripts/mvtech_run.sh
+++ tf2/scripts/mvtech_run.sh
@@ -1,10 +1,10 @@
 categories=[Bottle,Pill,Tile,Transistor,Zipper,Hazelnut,metal_nut,Toothbrush,Wood]
 
-python3 run.py --dataset=mvtech --data_dir="/mnt/runs/data" --eval_split=test \
---model_dir="/mnt/runs/logs" --mode=train_then_eval --train_mode=pretrain --image_size=224 \
---image_size=224 --train_batch_size=256 --lineareval_while_pretraining=True --test_perc=0.3 \
+python3 run.py --dataset=mvtech --data_dir="/mnt/dev/data" --eval_split=test \
+--model_dir="/mnt/dev/runs/logs" --mode=train_then_eval --train_mode=pretrain --image_size=224 \
+--image_size=224 --train_batch_size=256 --lineareval_while_pretraining=True --test_perc=0.5 \
 --eval_batch_size=256 --load_existing_split=False --anomaly_perc=0.15 --run_id=001 \
---train_epochs=200 # --categories=$categories
+--train_epochs=200 --eval_mahal=True # --categories=$categories
 
 
 
